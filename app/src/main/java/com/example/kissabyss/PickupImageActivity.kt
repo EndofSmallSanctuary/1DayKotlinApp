4f6c05cfdd93b134550280c7d171bf91c58235e3
EndofSmallSanctuary <teenagefeverhotline@gmail.com>
2022-01-15 13:21:28 +0300

    Apply Filter activity UI developed

---
+++ app/src/main/java/com/example/kissabyss/PickupImageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kissabyss
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PickupImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pickup_image)
+    }
+}

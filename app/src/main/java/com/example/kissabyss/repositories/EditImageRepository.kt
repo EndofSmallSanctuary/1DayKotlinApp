09a3bf0a3d48066b5e789e54230f0c06f0785bb8
EndofSmallSanctuary <teenagefeverhotline@gmail.com>
2022-01-16 11:43:51 +0300

    Added file deploy skeleton in Repository Implementation

--- app/src/main/java/com/example/kissabyss/repositories/EditImageRepository.kt
+++ app/src/main/java/com/example/kissabyss/repositories/EditImageRepository.kt
@@ -7,5 +7,5 @@ import com.example.kissabyss.processing.ImageFilter
 interface EditImageRepository {
     suspend fun decodeImageFromUri(imageUri: Uri): Bitmap?
     suspend fun getImageFilters(image: Bitmap) : List<ImageFilter>
-    suspend fun saveFilteredImage
+    suspend fun saveFilteredImage(filteredBitmap: Bitmap) : Uri?
 }
--- app/src/main/java/com/example/kissabyss/repositories/impls/EditImageRepositoryImpl.kt
+++ app/src/main/java/com/example/kissabyss/repositories/impls/EditImageRepositoryImpl.kt
@@ -4,10 +4,15 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Environment
+import androidx.core.content.FileProvider
 import com.example.kissabyss.processing.ImageFilter
 import com.example.kissabyss.repositories.EditImageRepository
+import com.example.kissabyss.utilities.displayToast
 import jp.co.cyberagent.android.gpuimage.GPUImage
 import jp.co.cyberagent.android.gpuimage.filter.*
+import java.io.File
+import java.io.FileOutputStream
 import java.io.InputStream
 
 class EditImageRepositoryImpl(private val context: Context) : EditImageRepository  {
@@ -421,5 +426,32 @@ class EditImageRepositoryImpl(private val context: Context) : EditImageRepositor
 
     private fun getInputStreamFromUri(uri: Uri): InputStream?{
         return context.contentResolver.openInputStream(uri)
+    }
+
+    override suspend fun saveFilteredImage(filteredBitmap: Bitmap): Uri? {
+        kotlin.runCatching {
+            val mediaStorageDirectory  = File(
+                context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "Kiss Saved Images"
+            )
+            if(!mediaStorageDirectory.exists()){
+                mediaStorageDirectory.mkdirs()
+            }
+            val fileName = "IMG_${System.currentTimeMillis()}.jpg"
+            val file = File(mediaStorageDirectory,fileName)
+            saveFile(file,filteredBitmap)
+           return FileProvider.getUriForFile(context,"${context.packageName}.provider",file)
+        }.onFailure { error->
+            context.displayToast(error.message)
+            return null
+        }
+        return null
+    }
+
+    private fun saveFile(file: File, bitmap: Bitmap){
+        with(FileOutputStream(file)){
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,this)
+            flush()
+            close()
+        }
     }
 }
